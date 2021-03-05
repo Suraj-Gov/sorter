0f0d8c727a85e31efe71a060cde02ea79e9d8d6a
Suraj Govind <surajgovind332000@gmail.com>
2021-03-05 18:01:08 +0530

    hello

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const Index = () => {
+  return <App />;
+};
+
+ReactDOM.render(<Index />, document.getElementById("root"));
