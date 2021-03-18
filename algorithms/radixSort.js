34c957bea39d665f8b000b8931e465000ebfb358
Suraj-Gov <surajgovind332000@gmail.com>
2021-03-18 21:41:19 +0530

    refactored radixSort

--- algorithms/radixSort.js
+++ algorithms/radixSort.js
@@ -2,34 +2,38 @@ let inputArr = Array.from(Array(10)).map((_) =>
   Math.floor(Math.random() * 900)
 );
 
-const maxElementDigits = inputArr
-  .reduce((retVal, curVal) => (curVal >= retVal ? curVal : retVal), -1)
-  .toString().length;
+const radixSort = () => {
+  const maxElementDigits = inputArr
+    .reduce((retVal, curVal) => (curVal >= retVal ? curVal : retVal), -1)
+    .toString().length;
 
-const generateStringifiedNum = (number, length) => {
-  let stringifiedNumber = number.toString();
-  while (stringifiedNumber.length < length) {
-    stringifiedNumber = `0${stringifiedNumber}`;
+  const generateStringifiedNum = (number, length) => {
+    let stringifiedNumber = number.toString();
+    while (stringifiedNumber.length < length) {
+      stringifiedNumber = `0${stringifiedNumber}`;
+    }
+    return stringifiedNumber;
+  };
+  let countArr = Array.from(Array(10)).map((_) => []);
+
+  for (let i = maxElementDigits - 1; i >= 0; i--) {
+    // const i = maxElementDigits - 1
+    for (let j = 0; j < inputArr.length; j++) {
+      const digit = parseInt(
+        generateStringifiedNum(inputArr[j], maxElementDigits).charAt(i)
+      );
+      countArr[digit].push(inputArr[j]);
+    }
+    let sortedArr = [];
+    countArr.forEach((digitArr) => {
+      while (digitArr.length > 0) {
+        sortedArr.push(digitArr.shift());
+      }
+    });
+    inputArr = [...sortedArr];
   }
-  return stringifiedNumber;
+  return inputArr;
 };
 
-let countArr = Array.from(Array(10)).map((_) => []);
-
-for (let i = maxElementDigits - 1; i >= 0; i--) {
-  // const i = maxElementDigits - 1
-  for (let j = 0; j < inputArr.length; j++) {
-    const digit = parseInt(
-      generateStringifiedNum(inputArr[j], maxElementDigits).charAt(i)
-    );
-    countArr[digit].push(inputArr[j]);
-  }
-  let sortedArr = [];
-  countArr.forEach((digitArr) => {
-    while (digitArr.length > 0) {
-      sortedArr.push(digitArr.shift());
-    }
-  });
-  console.log(sortedArr, `digit: ${i}`);
-  inputArr = [...sortedArr];
-}
+console.log(radixSort(inputArr));
+// idk how I can visualize this when the array is recreated based on the indexes
